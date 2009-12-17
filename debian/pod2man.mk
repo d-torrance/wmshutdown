06dfa1e876a6ae7f748cdb0ff58a7a78cc549783
Jari Aalto <jari.aalto@cante.net>
2009-12-17 11:57:41 +0200

    debian/pod2man.mk: update

--- debian/pod2man.mk
+++ debian/pod2man.mk
@@ -6,17 +6,18 @@
 #
 #   License
 #
-#	This program is free software; you can redistribute it and/or
-#	This program is free software; you can redistribute it and/or
-#	modify it under the terms of the GNU General Public License as
-#	published by the Free Software Foundation; either version 2 of the
-#	License, or (at your option) any later version
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful, but
-#	WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#	General Public License for more details at
-#	Visit <http://www.gnu.org/copyleft/gpl.html>.
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 #   Description
 #
@@ -49,6 +50,7 @@ POD2MAN_FLAGS	= --utf8
 makeman: $(MANPAGE)
 
 $(MANPAGE): $(MANPOD)
+	# make target - create manual page from a *.pod page
 	podchecker $(MANPOD)
 	LC_ALL= LANG=C $(POD2MAN) $(POD2MAN_FLAGS) \
 		--center="$(PODCENTER)" \

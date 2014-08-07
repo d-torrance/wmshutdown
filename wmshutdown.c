fa5e3d546f22bf6a0014cad42adf33a02f4b8f03
Doug Torrance <dtorrance@monmouthcollege.edu>
2014-08-06 22:45:31 -0500

    wmshutdown.c: Update copyright header; add COPYING file.

--- wmshutdown.c
+++ wmshutdown.c
@@ -1,13 +1,20 @@
-/*
- * wmshutdown.c
+/* wmshutdown - dockapp to shutdown or reboot your machine
  *
- * (C) 2001 Rafael V. Aroca <rafael@linuxqos.cjb.net>
+ * Copyright 2001, 2002 Rafael V. Aroca <rafael@linuxqos.cjb.net>
+ * Copyright 2014 Doug Torrance <dtorrance@monmouthcollege.edu>
  *
- * This software is under GPL
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Based on foodock lib by Alexey Vyskubov <alexey@pepper.spb.ru>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- */
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 #include <gtk/gtk.h>
 #include <gdk/gdkx.h>
@@ -17,8 +24,8 @@ GtkWidget *dialog = NULL;
 
 /* gtk3 dockapp code based on wmpasman by Brad Jorsch
  * <anomie@users.sourceforge.net>
- * http://sourceforge.net/projects/wmpasman/
- */
+ * http://sourceforge.net/projects/wmpasman */
+
 GtkWidget *cria_dock(GtkWidget *mw, unsigned int s) {
 	GdkDisplay *display;
 	GtkWidget *foobox;
